3a327b2d0d13e443713b720ab449755289be2b32
Ameer77-tech <ameershaik.cs@gmail.com>
2025-11-09 14:54:07 +0530

    created project and tasks models

---
+++ server/models/project.model.js
@@ -0,0 +1,33 @@
+import mongoose, { mongo } from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  dueDate: {
+    type: Date,
+    default: null,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  totaltasksCompleted: {
+    type: Number,
+    default: 0,
+  },
+  tasks: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "tasks",
+  },
+});
+
+const projectsModel = new mongoose.Model("projects", projectSchema);
+
+export default projectsModel;
---
+++ server/models/task.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    enum: ["personal", "project"],
+    default: "personal",
+  },
+  linkedProject: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "projects",
+    required: function () {
+      return this.type === "project";
+    },
+  },
+  dueDate: {
+    type: Date,
+  },
+  priority: {
+    type: Number,
+    enum: [1, 2, 3],
+    default: 1,
+  },
+  timer: {
+    type: Date,
+    required: true,
+    default: null,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const tasksModel = mongoose.model("tasks", taskSchema);
+export default tasksModel;
