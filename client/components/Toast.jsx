7484da04c537025f9e9e6f6700278bee294624ed
Ameer77-tech <ameershaik.cs@gmail.com>
2025-11-21 10:20:55 +0530

    made small changes on toast ui

--- client/components/Toast.jsx
+++ client/components/Toast.jsx
@@ -4,22 +4,20 @@ import { CheckCircle2, XCircle, Info } from "lucide-react";
 import clsx from "clsx";
 
 const Toast = ({ show, toastData }) => {
-  
-
   const { message, isSuccess, type } = toastData || {};
 
   const icons = {
-    success: <CheckCircle2 className="text-emerald-500" size={26} />,
-    error: <XCircle className="text-white-500" size={26} />,
-    info: <Info className="text-white" size={26} />,
+    success: <CheckCircle2 className="text-background" size={26} />,
+    error: <XCircle className="text-background" size={26} />,
+    info: <Info className="text-background" size={26} />,
   };
 
   const variant =
     type === "success"
-      ? "bg-emerald-600/90 text-white"
+      ? "bg-primary text-background"
       : type === "error"
-      ? "bg-red-600/90 text-white"
-      : "bg-blue-600/90 text-white";
+      ? "bg-red-600/90 text-background"
+      : "bg-blue-600/90 text-background";
 
   return (
     <AnimatePresence>
@@ -30,7 +28,7 @@ const Toast = ({ show, toastData }) => {
           exit={{ y: -50, opacity: 0, scale: 0.9 }}
           transition={{ type: "spring", stiffness: 300, damping: 20 }}
           className={clsx(
-            "fixed top-6 left-1/2 -translate-x-1/2 z-[9999] flex items-center gap-3 px-5 py-3 rounded-2xl shadow-2xl backdrop-blur-md border border-white/10",
+            "fixed top-6 left-1/2 -translate-x-1/2 z-9999 flex items-center gap-3 px-5 py-3 rounded-2xl shadow-2xl backdrop-blur-md border border-white/10",
             variant
           )}
         >
